b0a7d0add284737564e83d98dfd730c952ee1467
Bejaoui Karim <kbejaoui@sqli.com>
2017-09-20 16:56:35 +0200

    Affichage du résultat

---
+++ hulk/animation.ino
@@ -0,0 +1,88 @@
+
+int iteration = 0;
+
+void animate() {
+  // Tous les 4 itération on change d'animation
+  int animation = 0;
+  if (iteration > 10) {
+    iteration = 0;
+  }
+  if (iteration == 0 || iteration == 1 || iteration == 2 || iteration == 3) {
+    k2000(150);
+  } else if ( iteration == 4 || iteration == 5 || iteration == 6 || iteration == 7) {
+    chase(100);
+  } else if (iteration == 8) {
+    blinkAll(100);
+  } else {
+    blinkEvenOddl(100);
+  }
+  iteration++;
+}
+
+void turnOn(int rank) {
+  chase(20);
+  chase(50);
+  chase(80);
+  chase(110);
+  chase(140);
+  chase(170);
+  chase(200);
+  for (int ledPin = 2; ledPin <= rank+1; ledPin++) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+    delay(200);                  // waits for a second
+    digitalWrite(ledPin, LOW);    // sets the LED off
+  }
+  digitalWrite(rank+1, HIGH);   // sets the LED on
+}
+
+void chase(int delaySeconds) {
+  for (int ledPin = 2; ledPin <= 11; ledPin++) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+    delay(delaySeconds);                  // waits for a second
+    digitalWrite(ledPin, LOW);    // sets the LED off
+  }
+}
+
+void k2000(int delaySeconds) {
+  for (int ledPin = 2; ledPin <= 10; ledPin++) {
+    digitalWrite(ledPin - 1, LOW); // sets the LED on
+    digitalWrite(ledPin, HIGH);    // sets the LED off
+    digitalWrite(ledPin + 1, HIGH); // sets the LED on
+    delay(delaySeconds);                  // waits for a second
+  }
+  for (int ledPin = 10; ledPin >= 2; ledPin--) {
+    digitalWrite(ledPin + 1, LOW); // sets the LED on
+    digitalWrite(ledPin, HIGH);    // sets the LED off
+    digitalWrite(ledPin - 1, HIGH); // sets the LED on
+    delay(delaySeconds);                  // waits for a second
+  }
+}
+
+void blinkAll(int delaySeconds) {
+  for (int ledPin = 2; ledPin <= 11; ledPin++) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+  }
+  delay(delaySeconds);                  // waits for a second
+  for (int ledPin = 2; ledPin <= 11; ledPin++) {
+    digitalWrite(ledPin, LOW);    // sets the LED off
+  }
+  delay(delaySeconds);
+}
+
+void blinkEvenOddl(int delaySeconds) {
+  for (int ledPin = 2; ledPin <= 11; ledPin = ledPin + 2) {
+    digitalWrite(ledPin, HIGH);   // sets the LED on
+  }
+  delay(delaySeconds);                  // waits for a second
+  for (int ledPin = 3; ledPin <= 11; ledPin = ledPin + 2) {
+    digitalWrite(ledPin, LOW);    // sets the LED off
+  }
+  delay(delaySeconds);
+}
+
+void results(int count, int delaySeconds) {
+  chase(delaySeconds);
+  chase(delaySeconds);
+}
+
+
--- hulk/hulk.ino
+++ hulk/hulk.ino
@@ -1,8 +1,8 @@
 /*
   Permet de lire une valeur de tension dans le cadre du jeu Hulk
   Cette valeur correspond à la valeur d'impédance du corps humain.
-  Utilisation d'un 
- */
+  Utilisation d'un
+*/
 
 bool mesure = false;
 
@@ -10,87 +10,43 @@ bool mesure = false;
 void setup() {
   // Communication serial en 9600 baud
   Serial.begin(9600);
-  for (int ledPin=2; ledPin <= 11; ledPin++){
-    pinMode(ledPin, OUTPUT);   
-   } 
+  for (int ledPin = 2; ledPin <= 11; ledPin++) {
+    pinMode(ledPin, OUTPUT);
+  }
 }
 
 // la boucle qui tourne indéfinément
 void loop() {
-  
   // S'il y a des données
   if (Serial.available() > 0) {
-    chase(100);
     // On lit les données sur le port serie
-       String command = Serial.readString();
-       // Si les données correspondent à la chaine "START"
-       if (command == "START"){
-          chase(20);
-          int sensorValue = 0;
-          do{
-             // Lit la valeur sur A0 ( 0 - 1023)
-            sensorValue = analogRead(A0);
-          chase(10);
-          } while (sensorValue<100);
-         
-          // On envoi la réponse sur le port Serial
-          Serial.println(sensorValue);
-          chase(20);
-          digitalWrite(2, LOW);   // sets the LED on
-          digitalWrite(3, HIGH);  
-          digitalWrite(4, LOW); 
-          mesure =true; 
-        }
-        if (command == "STOP"){
-          mesure =false; 
-        }
-   }
-   if (!mesure){
-    if (random(4) == 1){
-          k2000(100);
-          k2000(100);
-          k2000(100);
-    }else{
-      chase(100);
+    String command = Serial.readString();
+    // Si les données correspondent à la chaine "START"
+    if (command == "START") {
+      chase(20);
+      int sensorValue = 0;
+      do {
+        // Lit la valeur sur A0 ( 0 - 1023)
+        sensorValue = analogRead(A0);
+        chase(10);
+      } while (sensorValue < 100);
+
+      // On envoi la réponse sur le port Serial
+      Serial.println(sensorValue);
+      mesure = true;
     }
-   }
-}
-
-void chase(int delaySeconds){
-  for (int ledPin=2; ledPin <= 11; ledPin++){
-     digitalWrite(ledPin, HIGH);   // sets the LED on
-     delay(delaySeconds);                  // waits for a second
-     digitalWrite(ledPin, LOW);    // sets the LED off
-  } 
-}
-
-void k2000(int delaySeconds){
-  for (int ledPin=2; ledPin <= 10; ledPin++){
-     digitalWrite(ledPin-1, LOW);   // sets the LED on
-     digitalWrite(ledPin, HIGH);    // sets the LED off
-     digitalWrite(ledPin+1, HIGH);   // sets the LED on
-     delay(delaySeconds);                  // waits for a second
-  } 
-  for (int ledPin=10; ledPin >= 2; ledPin--){
-     digitalWrite(ledPin-1, LOW);   // sets the LED on
-     digitalWrite(ledPin, HIGH);    // sets the LED off
-     digitalWrite(ledPin+1, HIGH);   // sets the LED on
-     delay(delaySeconds);                  // waits for a second
-     
-  } 
-}
-
-void blinkAll(int delaySeconds){
-  for (int ledPin=2; ledPin <= 11; ledPin++){
-    digitalWrite(ledPin, HIGH);   // sets the LED on
+    else if (command == "STOP") {
+      mesure = false;
+    }
+    else{
+      mesure = true;
+      turnOn(command.toInt());
+    }
+    
   }
-     delay(delaySeconds);                  // waits for a second
-     for (int ledPin=2; ledPin <= 11; ledPin++){
-     digitalWrite(ledPin, LOW);    // sets the LED off
-     }
-     delay(delaySeconds);
+  if (!mesure) {
+    animate();
+  }
 }
-
-
 
 
